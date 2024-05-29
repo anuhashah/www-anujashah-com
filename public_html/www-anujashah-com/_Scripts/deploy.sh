6338adb9d0cb9ed4413055156da46166058c896d
Anuja Shah <apshah@usc.edu>
2024-05-28 17:38:54 -0700

    deployment script

---
+++ public_html/www-anujashah-com/_Scripts/deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Navigate to your remote repository
+cd /path/to/remote/repository.git
+
+# Pull changes from your local repository
+git --git-dir=/Users/anujashah/Desktop/Coding/www-anujashah-com.git --work-tree=/Users/anujashah/Desktop/Coding/www-anujashah-com pull origin master
