977d2db611fad75c43347e9f69dae7064aa6f2db
Anuja Shah <apshah@usc.edu>
2024-05-28 18:21:10 -0700

    deployment secret

--- public_html/www-anujashah-com/_Scripts/deploy.sh
+++ public_html/www-anujashah-com/_Scripts/deploy.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
-# Set your secret token
-SECRET_TOKEN="HotToddy2"
+# # Set your secret token
+# SECRET_TOKEN="HotToddy2"
 
-# Get the secret token from the request headers or payload
-WEBHOOK_SECRET_TOKEN="${HTTP_X_HUB_SIGNATURE_256}"
+# # Get the secret token from the request headers or payload
+# WEBHOOK_SECRET_TOKEN="${HTTP_X_HUB_SIGNATURE_256}"
 
-# Compare the secret tokens
-if [ "$SECRET_TOKEN" == "$WEBHOOK_SECRET_TOKEN" ]; then
-    # Pull changes from your local repository
-	git --git-dir=/Users/anujashah/Desktop/Coding/www-anujashah-com.git --work-tree=/Users/anujashah/Desktop/Coding/www-anujashah-com pull origin master
-fi
+# # Compare the secret tokens
+# if [ "$SECRET_TOKEN" == "$WEBHOOK_SECRET_TOKEN" ]; then
+#     # Pull changes from your local repository
+# 	git --git-dir=/Users/anujashah/Desktop/Coding/www-anujashah-com.git --work-tree=/Users/anujashah/Desktop/Coding/www-anujashah-com pull origin master
+# fi
 
-#HEHE
+# #HEHE
+
+
+# Navigate to your remote repository
+cd ~/home4/anujasha
+
+# Pull changes from your local repository
+git --git-dir=/Users/anujashah/Desktop/Coding/www-anujashah-com/.git --work-tree=/Users/anujashah/Desktop/Coding/www-anujashah-com pull origin master
