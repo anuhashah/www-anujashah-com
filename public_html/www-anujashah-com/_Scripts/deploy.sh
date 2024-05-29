8ff35a0c9a6c5e0738c3c65f3e9d1ad08838ebea
Anuja Shah <apshah@usc.edu>
2024-05-28 17:55:34 -0700

    deployment secret

--- public_html/www-anujashah-com/_Scripts/deploy.sh
+++ public_html/www-anujashah-com/_Scripts/deploy.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-# Navigate to your remote repository
-cd /path/to/remote/repository.git
+# Set your secret token
+SECRET_TOKEN="HotToddy2"
 
-# Pull changes from your local repository
-git --git-dir=/Users/anujashah/Desktop/Coding/www-anujashah-com.git --work-tree=/Users/anujashah/Desktop/Coding/www-anujashah-com pull origin master
+# Get the secret token from the request headers or payload
+WEBHOOK_SECRET_TOKEN="${HTTP_X_HUB_SIGNATURE_256}"
+
+# Compare the secret tokens
+if [ "$SECRET_TOKEN" == "$WEBHOOK_SECRET_TOKEN" ]; then
+    # Pull changes from your local repository
+	git --git-dir=/Users/anujashah/Desktop/Coding/www-anujashah-com.git --work-tree=/Users/anujashah/Desktop/Coding/www-anujashah-com pull origin master
+fi
